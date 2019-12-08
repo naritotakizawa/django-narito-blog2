b2a75edf8d9c8a975e4fe3f6d02df007f582c663
unknown <toritoritorina@gmail.com>
2019-12-09 03:49:29 +0900

    検索結果に応じて見出しをかえた

--- nblog2/views.py
+++ nblog2/views.py
@@ -11,18 +11,33 @@ class NoteList(generic.ListView):
     ordering = '-created_at'
 
     def get_queryset(self):
+        self.category = self.key_word = ''
         queryset = super().get_queryset()
         form = NoteSearchForm(self.request.GET or None)
         if form.is_valid():
-            category = form.cleaned_data.get('category')
+            category = self.category = form.cleaned_data.get('category')
             if category:
                 queryset = queryset.filter(category=category)
 
-            key_word = form.cleaned_data['key_word']
+            key_word = self.key_word = form.cleaned_data['key_word']
             if key_word:
                 queryset = queryset.filter(Q(title__icontains=key_word) | Q(page__text__icontains=key_word)).distinct()
 
         return queryset.select_related('category')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        heading = ''
+        if self.category:
+            heading += '「{}」'.format(self.category.name)
+        if self.key_word:
+            heading += '「{}」'.format(self.key_word)
+        if heading:
+            heading += 'の検索結果'
+        else:
+            heading = '全てのノート'
+        context['heading'] = heading
+        return context
 
 
 class NoteDetail(generic.DetailView):
